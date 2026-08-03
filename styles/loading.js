ce5c78e05c04fa08dde0007c6fac63063a6ba8ee
stasyabis7-crypto <stasyabis7@gmail.com>
2026-08-03 16:34:39 +0300

    update

---
+++ styles/loading.js
@@ -0,0 +1,49 @@
+(function () {
+  var root = document.documentElement;
+  var items = document.querySelectorAll('.masonry__item');
+
+  function reveal(item) {
+    if (item) item.classList.add('is-media-loaded');
+  }
+
+  items.forEach(function (item) {
+    var media = item.querySelector('iframe, img');
+    if (!media) {
+      reveal(item);
+      return;
+    }
+
+    var ready = media.tagName === 'IMG'
+      ? media.complete && media.naturalWidth > 0
+      : false;
+
+    if (media.tagName === 'IFRAME') {
+      try {
+        ready = media.contentDocument && media.contentDocument.readyState === 'complete';
+      } catch (_) {
+        ready = false;
+      }
+    }
+
+    if (ready) reveal(item);
+    else media.addEventListener('load', function () { reveal(item); }, { once: true });
+  });
+
+  var fontsReady = document.fonts && document.fonts.ready
+    ? document.fonts.ready.catch(function () {})
+    : Promise.resolve();
+  var avatar = document.querySelector('.site-header__avatar img');
+  var avatarReady = avatar && avatar.decode
+    ? avatar.decode().catch(function () {})
+    : Promise.resolve();
+  var safetyTimeout = new Promise(function (resolve) { setTimeout(resolve, 3000); });
+
+  Promise.race([
+    Promise.all([fontsReady, avatarReady]),
+    safetyTimeout
+  ]).then(function () {
+    requestAnimationFrame(function () {
+      requestAnimationFrame(function () { root.classList.remove('is-page-loading'); });
+    });
+  });
+})();
